4f88137accc8ed9bb75bb92d3706c647df511911
Mira <dudova_2001@mail.ru>
2023-03-15 14:35:30 +0300

    Add code main

--- progra.cpp
+++ progra.cpp
@@ -4,3 +4,15 @@
 #include "conio.h"
 using namespace std;
 int main()
+{
+    int getch();
+    setlocale(LC_ALL, "Russian");
+    int x, y;
+    printf("Введите Х:");
+    scanf_s("%d", &x);
+    printf("Введите Y:");
+    scanf_s("%d", &y);
+
+    int i = y;
+    int c = 0;
+    int d = 0;
